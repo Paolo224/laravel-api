cadf5b285efae60ffc33315820133b5522e4c729
Paolo224 <picariellopaolo224@gmail.com>
2023-03-06 17:28:56 +0100

    inserisco i seeder nel DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,6 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TypesTableSeeder::class,
             TechnologiesSeeder::class,
+            RoleSeeder::class,
+            RoleUserSeeder::class,
             ProjectsTableSeeder::class,
             ProjectsTechnologiesSeeder::class,
         ]);
